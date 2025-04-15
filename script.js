de4cba9cbd287160eb235358ac89e36e4964be84
MassayoshiDev <psnnapoleandro@gmail.com>
2025-04-15 11:51:31 -0300

    update - inicio do js

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function print(msg) {
     console.log(msg)
 }
 
+
+function setVet(tid) {
+    tid += 1
+    if (tid == 0) {
+        return document.getElementById("formSeu")
+    } else {
+        return document.getElementById("formDele")
+    }
+}
+
+function checkValuesInBothForms(oid) {
+    var form = setVet(oid)
+    print(form.inNome.value)
+}
+
 function validarForm() {
-    alert("Formulário enviado.")
+    let id = -1
+    checkValuesInBothForms(id)
+    id = 0
+    checkValuesInBothForms(id)
 }
